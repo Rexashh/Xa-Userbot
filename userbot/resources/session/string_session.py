3b0dd11ef391e09b06f66f62efe0fde9861e6d83
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-02-15 01:23:39 +0700

    Update string_session.py

--- userbot/resources/session/string_session.py
+++ userbot/resources/session/string_session.py
@@ -14,7 +14,7 @@ API_HASH = input("Enter API_HASH here: ")
 with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
     print("Check Telegram Save Message Mu Untuk Copy STRING_SESSION ")
     session_string = client.session.save()
-    saved_messages_template = """Grup Support @Rose_Userbot
+    saved_messages_template = """Grup Support @rexaprivateroom
 <code>STRING_SESSION</code>: <code>{}</code>
 ⚠️ <i>Please be careful before passing this value to third parties</i>""".format(
         session_string
