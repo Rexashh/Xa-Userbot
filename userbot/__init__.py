5e7aef5866bd2223ed02eceddae38661292756f1
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-02-15 16:21:17 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -155,7 +155,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/Rexashh/Xa-Userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "Xa-Userbot")
+    "UPSTREAM_REPO_BRANCH", "master")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
