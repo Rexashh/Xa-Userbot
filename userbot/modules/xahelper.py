c3f23d44c651fa06e9277b4023841f2c95ee8575
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-02-18 02:07:10 +0700

    Update xahelper.py

--- userbot/modules/xahelper.py
+++ userbot/modules/xahelper.py
@@ -12,22 +12,22 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/IDnyaKosong)"
-        "\n[Repo](https://github.com/S/Kyy-Userbot)"
-        "\n[Instagram](instagram.com/rizkyhamdanii16_)")
+        "\n[Telegram](t.me/JustRex)"
+        "\n[Repo](https://github.com/S/Xa-Userbot)"
+        "\n[Instagram](instagram.com/syhndr_)")
 
 
 @register(outgoing=True, pattern="^.vars$")
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/muhammadrizky16/Kyy-Userbot/Kyy-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Rexashh/Xa-Userbot/Xa-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
     "`.lhelp`\
-\nUsage: Bantuan Untuk Rose-Userbot.\
+\nUsage: Bantuan Untuk Xa-Userbot.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
 })
