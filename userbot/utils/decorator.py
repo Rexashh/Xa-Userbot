dc7dab9427f2d28afa2506dbc0de48d97ceaf7e7
muhammadrizky16 <malikkudus369@gmail.com>
2022-02-17 13:05:21 +0000

    pylint: auto fixes
    Signed-off-by: muhammadrizky16 <malikkudus369@gmail.com>

--- userbot/utils/decorator.py
+++ userbot/utils/decorator.py
@@ -55,7 +55,7 @@ def Xa_cmd(
         else:
             xa_ = "\\" + CMD_HANDLER
             sudo_ = "\\" + SUDO_HANDLER
-            xa_reg = re.compile(kyy_ + pattern)
+            re.compile(kyy_ + pattern)
             sudo_reg = re.compile(sudo_ + pattern)
             if command is not None:
                 cmd1 = kyy_ + command
