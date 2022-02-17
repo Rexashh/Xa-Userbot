12ee5c72f3d3e941f405db33f1f5626000c7c10f
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-02-18 03:09:25 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -140,6 +140,7 @@ async def permitpm(event):
                     )
 
 
+                    
 @register(disable_edited=True, outgoing=True, disable_errors=True)
 async def auto_accept(event):
     """Will approve automatically if you texted them first."""
