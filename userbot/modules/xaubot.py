caf46bfaaca1a8243ccc1b377261652a8c03d6c8
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-02-18 16:44:00 +0700

    Update xaubot.py

--- userbot/modules/xaubot.py
+++ userbot/modules/xaubot.py
@@ -176,7 +176,7 @@ async def typewriter(typew):
 
 
 CMD_HELP.update({
-    "kyyubot": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}sadboy`\
+    "Xaubot": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}sadboy`\
     \n↳ : Biasalah sadboy hikss\
     \n\n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}wah`\
     \n↳ : Ngatain orang war\
