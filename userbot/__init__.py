afd8d4704e23e97da3e1e6b498a494920a97063f
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-03-22 13:19:34 +0700

    Fixed pybase64

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -433,8 +433,8 @@ except Exception as e:
 
 
 async def checking():
-    gocheck = str(pybase64.b64decode("QHRpcmV4Z3VnZWw="))[2:15]
-    checker = str(pybase64.b64decode("QHJleGFwcml2YXRlcm9vbQ=="))[2:16]
+    gocheck = str(pybase64.b64decode("QHJleGFwcml2YXRlcm9vbQ=="))[2:15]
+    checker = str(pybase64.b64decode("QHRpcmV4Z3VnZWw="))[2:16]
     try:
         await bot(GetSec(gocheck))
     except BaseException:
