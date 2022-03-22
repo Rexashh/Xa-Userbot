749774b680f922bda1bf1150d850a46d9af8df43
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-03-22 09:28:13 +0700

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -35,8 +35,8 @@ clear
 pkg update && pkg upgrade -y
 pkg install clang curl git libcrypt libffi libiconv libjpeg* libjpeg-turbo libwebp libxml2 libxslt make ndk-sysroot openssl postgresql python readline wget zlib -y
 
-git clone https://github.com/muhammadrizky16/Kyy-Userbot.git
-cd Kyy-Userbot
+git clone https://github.com/Rexashh/Xa-Userbot.git
+cd Xa-Userbot
 
 pip install --upgrade pip setuptools
 pip install -r requirements.txt
