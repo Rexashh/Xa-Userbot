338a59245a6330baea3f2ceaf87d395602d3c928
ҡʏᴜʀᴧ <96605016+Kyuraxp@users.noreply.github.com>
2022-03-24 17:24:30 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -188,8 +188,8 @@ async def bash(cmd):
 
 def post_to_telegraph(title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "Kyy-Userbot"
-    auth_url = "https://github.com/muhammadrizky16/Kyy-Userbot"
+    auth_name = "Xa-Userbot"
+    auth_url = "https://github.com/Rexashh/Xa-Userbot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=title,
