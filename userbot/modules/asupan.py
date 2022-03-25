e142ccc1c303103e135385c268270262db989188
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-03-25 11:59:54 +0700

    up ayang & desah from hiroshi thx

--- userbot/modules/asupan.py
+++ userbot/modules/asupan.py
@@ -38,18 +38,18 @@ async def _(event):
         desahnya = [
             desah
             async for desah in event.client.iter_messages(
-                "@DESAHANFCE", filter=InputMessagesFilterVoice
+                "@deshanhiroshi", filter=InputMessagesFilterVoice
             )
         ]
         aing = await event.client.get_me()
         await event.client.send_file(
             event.chat_id,
             file=random.choice(desahnya),
-            caption=f"Nih kak desahannya [{owner}](tg://user?id={aing.id})",
+            caption=f"Jangan Sange ya kak, nih desah buat [{owner}](tg://user?id={aing.id})",
         )
         await event.delete()
     except Exception:
-        await event.edit("Tidak bisa menemukan desahan.")
+        await event.edit("sange nya ditahan dulu ya kak.")
 
 
 @Xa_cmd(pattern="vidlucu$")
@@ -71,16 +71,37 @@ async def _(event):
     except Exception:
         await event.edit("Tidak bisa menemukan video lucu.")
 
+@Xa_cmd(pattern="ayang$")
+async def _(event):
+    try:
+        ayangnya = [
+            ayang
+            async for ayang in event.client.iter_messages(
+                "@IndomieGantengV2", filter=InputMessagesFilterPhotos
+            )
+        ]
+        aku = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(ayangnya),
+            caption=f"**Ayang nya** [{aku.first_name}](tg://user?id={aku.id})")
+
+        await event.delete()
+    except Exception:
+        await event.edit("**GA ADA YANG MAU SAMA LO, MAKANYA CAKEP!.**")
+
 
 CMD_HELP.update(
     {
         "asupan": f"**Plugin : **asupan\
-        \n\n  •  **Syntax :** {cmd}asupan\
-        \n  •  **Function : **Untuk Mengirim video asupan secara random.\
-        \n\n  •  **Syntax :** {cmd}desah\
-        \n  •  **Function : **Untuk Mengirim voice desah secara random.\
-        \n\n  •  **Syntax :** {cmd}vidlucu\
-        \n  •  **Function : **Untuk Mengirim video lucu secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}asupan\
+        \n  ⌬  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mengirim video asupan secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}desah\
+        \n  ⌬  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mengirim voice desah secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}vidlucu\
+        \n  ⌬  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mengirim video lucu secara random.\
+        \n\n  •  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** {cmd}ayang\
+        \n  ⌬  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Untuk Mendapatkan Ayang mu, hehe.\
 "
     }
 )
