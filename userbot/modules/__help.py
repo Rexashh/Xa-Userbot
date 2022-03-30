f8b4ec4f4cdfb24bc51ec657d5546a56757434bd
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-03-30 17:07:04 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -24,7 +24,7 @@ from userbot.utils import edit_or_reply, edit_delete, Xa_cmd
 
 user = bot.get_me()
 DEFAULTUSER = user.first_name
-CUSTOM_HELP_EMOJI = "✙"
+CUSTOM_HELP_EMOJI = "⌬"
 
 
 @Xa_cmd(pattern="help ?(.*)")
@@ -32,7 +32,7 @@ async def cmd_list(event):
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await edit_or_reply(event, f"**✙ Commands available in {args} ✙** \n\n" + str(CMD_HELP[args]) + "\n\n**☞ @tirexgugel**")
+            await edit_or_reply(event, f"**⌬ Commands available in {args} ⌬** \n\n" + str(CMD_HELP[args]) + "\n\n**© @tirexgugel**")
         else:
             await edit_delete(event, f"**Module** `{args}` **Tidak tersedia!**")
     else:
