cf7050009672c43c4141c51ff43dd6dd68fdeab9
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-30 16:29:10 +0700

    revert

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -661,7 +661,7 @@ with bot:
             builder = event.builder
             result = None
             query = event.text
-            if event.query.user_id == uid and query.startswith("@KyyUserbot"):
+            if event.query.user_id == uid and query.startswith("@XaUserbot"):
                 buttons = paginate_help(0, dugmeler, "helpme")
                 result = builder.photo(
                     file=xalogo,
