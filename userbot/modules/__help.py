bd7bbca2a95373dd2b4e4443b8815c0c7e2f6bcb
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-03-30 15:58:32 +0700

    Update __help.py

--- userbot/modules/__help.py
+++ userbot/modules/__help.py
@@ -38,7 +38,7 @@ async def cmd_list(event):
     else:
         try:
             results = await bot.inline_query(  # pylint:disable=E0602
-                BOT_USERNAME, "@XaUserbot"
+                BOT_USERNAME, "@KyyUserbot"
             )
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
