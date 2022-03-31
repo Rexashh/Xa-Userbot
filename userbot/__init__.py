97279eae324523f3fd4b48cc453bfba80a43cff5
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-03-31 21:05:19 +0700

    fix pmpermit pic

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -158,7 +158,7 @@ PMPERMIT_TEXT = os.environ.get("PMPERMIT_TEXT", None)
 
 # Custom Pmpermit pic
 PMPERMIT_PIC = os.environ.get(
-    "PMPERMIT_PIC") or "https://telegra.ph/file/276d22aac9f400898cd27.jpg"
+    "PMPERMIT_PIC") or "https://telegra.ph/file/a25712fa68c951e3cd285.jpg"
 
 # Bleep Blop, this is a bot ;)
 PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
