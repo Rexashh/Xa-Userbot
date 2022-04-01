24f16e602319eabddd343d87302d0f73d70f36c1
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-04-02 00:41:10 +0700

    try aboutxa

---
+++ userbot/modules/aboutxa.py
@@ -0,0 +1,117 @@
+from time import sleep
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import Xa_cmd
+
+
+@Xa_cmd(pattern="xaintro(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**HAI!!**")
+    sleep(1.5)
+    await typew.edit("**Sebelumnya mau bilang**")
+    sleep(1.5)
+    await typew.edit("**TERIMAKASIH YANG SUDAH MENGGUNAKAN XA-USERBOT**")
+    sleep(2)
+    await typew.edit("**Maaf Jika buatan Saya tidak sebagus yang lain **")
+    sleep(2)
+    await typew.edit("**Tapi Saya Berusaha Untuk membuat kalian nyamam dengan buatan saya**")
+    sleep(2)
+    await typew.edit("**Bye the way**")
+    sleep(1.5)
+    await typew.edit("**Saya Asal Kota Bekasi**")
+    sleep(1.5)
+    await typew.edit("**Dan Umur Saya 20 Tahun**")
+    sleep(1.5)
+    await typew.edit("**Hmmmmm**")
+    sleep(1.5)
+    await typew.edit("**Udah gitu aja sih**")
+    sleep(1.5)
+    await typew.edit("**Lanjut ke Perintah yang lain yakk, **")
+    sleep(1.5)
+    await typew.edit("**Terimakasih**")
+
+
+@Xa_cmd(pattern="xaintro2(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit(
+        "**HALO!!!!Nama Saya Rexa Aditya, Saya pembuat Xa-Userbot hehehe,  asal Kota bekasi, yang satu kota yolah meet wkwk, Bang lu pasti jago coding?? Nope!!! Itu salah bgt, Gua cuma belajar otodidak, terus merhatiin orang orang, Ya pastinya gua juga dibantuin para suhu userbot, btw nih ya Kalo misalnya ada bug atau apapun itu kirim aja ya di gc support ku, atau butuh bantuan silahkan chat aja jgn sungkan**")
+
+
+@Xa_cmd(pattern="xaintro3(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Okeee di perintah ini saya mau Sharing tentang First Time saya bermain telegram**")
+    sleep(1.5)
+    await typew.edit("**Pertama kali saya download Telegram hanya untuk download film, Drakor dan Anime**")
+    sleep(1.5)
+    await typew.edit("**Dan makin lama makin bosen, Dan akhirnya gua coba main anonymous bot**")
+    sleep(1)
+    await typew.edit("**Karena Anonymous makin aneh, u knowlah wkwkwk**")
+    sleep(1)
+    await typew.edit("**akhirnya gua memberanikan diri untuk os digc mutualan wkwk**")
+    sleep(1)
+    await typew.edit("**Ternyata asik anjg wkwkwk**")
+    sleep(1)
+    await typew.edit("**Terus gua mulai di ajak main ke gc Family dan diajak buat make tittle mereka**")
+    sleep(1)
+    await typew.edit("**Udah sih singkatnya gitu aja wkwkw, Ga ada yang menarik dari gua**")
+    sleep(1)
+    await typew.edit("**kalo awal gua main bot karena gua merasa asik sama bot Rose Dan music milik hiroshi**")
+    sleep(1.5)
+    await typew.edit("**Gua ga tau apa itu userbot awalnya, sampai ada anak aliansi mainin Clone pake userbot**")
+    sleep(1)
+    await typew.edit("**akhirnya gua tanya itu bot apa**")
+    sleep(1)
+    await typew.edit("**dan akhirnya gua dikasih tau, tapi dia ga ngasih gua tutornya, ya akhrinya gua otodidak**")
+    sleep(1.5)
+    await typew.edit("**Intinya Repo Userbot pertama kali yg gua pake itu King Userbot**")
+    sleep(1)
+    await typew.edit("**Repo Music yang pertama kali gua pake itu levina**")
+    sleep(1)
+    await typew.edit( gitu aja sih makasih wkwkwk**")
+    
+    
+    
+@Xa_cmd(pattern="xaintro3(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**DISINI GA ADA APA APA WKWKWK**")
+
+
+@Xa_cmd(pattern="xaproject(?: |$)(.*)")
+async def project_xa(wannasee):
+    await wannasee.edit(
+        f"⌬ **xᴀ ᴍᴜsɪᴄ       :** [ᴋʟɪᴋ ᴅɪsɪɴɪ](https://t.me/xamusicrobot)\n"
+        f"⌬ **ʜɪᴋᴀʀɪ ʀᴏʙᴏᴛ    :** [ᴋʟɪᴋ ᴅɪsɪɴɪ](https://t.me/HikariManageRobot)\n"
+        f"⌬ **xᴀ sᴛʀɪɴɢ ʀᴏʙᴏᴛ :** [ᴋʟɪᴋ ᴅɪsɪɴɪ](https://t.me/xastringrobot)\n"
+        f"sᴇᴍᴜᴀ ʙᴏᴛ sᴇʟᴀʟᴜ sᴀʏᴀ ᴜᴘᴅᴀᴛᴇ"\n"
+        f"ᴘᴇʀɪᴋsᴀ ᴄʜᴀɴɴᴇʟ :** [xᴀ ᴘʀᴏᴊᴇᴄᴛ](https://t.me/tirexgugel)")
+        
+       
+@Xa_cmd(pattern="xabio(?: |$)(.*)")
+async def bio_xa(bioxa):
+    await bioxa.edit(
+        f"👨‍💻 **Nama         : Rexa Aditya\n"
+        f"📅 **TTL          : Bekasi, 20 September 2001\n"
+        f"🎸 **Genre Music  : Pop punk, Metal, Rap, Lofi\n"
+        f"👓 **Hobi         : Music, Novel, Film, Anime\n"
+        f"🌍 **Instagram    :**[klik Disini](https://www.instagram.com/syhndr_/)\n"
+        f"🎵 **Telegram     :**[Klik Disini](https://t.me/JustRex)\n"
+        f"👨‍🚀 **Tiktok       :**[Klik Disini](https://vt.tiktok.com/ZSd2tSP21/)\n"
+        f"Untuk lebih jelasnya tanya aja sendiri")
+        
+               
+CMD_HELP.update(
+    {
+        "aboutxa": f"**Plugin : **`aboutxa`\
+        \n\n    **Perintah :** `{cmd}xaintro1`\
+        \n⌬    **Fungsi : **Perkenalan tentang owner Xa-Userbot.\
+        \n\n    **Perintah :** `{cmd}xaintro2`\
+        \n⌬    **Fungsi : **Perkenalan tentang owner Xa-Userbot 2.\
+        \n\n    **Perintah :** `{cmd}xaintro3`\   
+        \n⌬    **Fungsi : **Perkenalan Tentang Owner Xa-Userbot 3.\
+        \n\n    **Perintah :**`{cmd}xaproject`\
+        \n⌬    **Fungsi : **Semua Tentang Project Bot Xa.\
+        \n\n    **Perintah :**`{cmd}xabio`\
+        \n⌬    **Fungsi : **Biodata Owner Xa-Userbot."
