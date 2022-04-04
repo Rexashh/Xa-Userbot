90acadbf2dd28f0039cb6cf22eb9a2a48d1ca192
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-04-05 05:33:15 +0700

    fix git from man

--- userbot/modules/github.py
+++ userbot/modules/github.py
@@ -1,50 +1,79 @@
+# Copyright (C) 2021 Catuserbot <https://github.com/sandy1709/catuserbot>
+# Ported by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import os
+
 import aiohttp
-from userbot.utils import Xa_cmd
-from userbot import CMD_HELP, CMD_HANDLER as cmd
+import requests
+from pySmartDL import SmartDL
+
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import edit_delete, edit_or_reply, Xa_cmd, reply_id
+
+ppath = os.path.join(os.getcwd(), "temp", "githubuser.jpg")
 
 
-@Xa_cmd(pattern="git (.*)")
-async def github(event):
-    URL = f"https://api.github.com/users/{event.pattern_match.group(1)}"
-    await event.get_chat()
-    async with aiohttp.ClientSession() as session:
-        async with session.get(URL) as request:
-            if request.status == 404:
-                return await event.reply("`" + event.pattern_match.group(1) +
-                                         " not found`")
+@Xa_cmd(pattern="github( -l(\d+))? ([\s\S]*)")
+async def _(event):
+    reply_to = await reply_id(event)
+    username = event.pattern_match.group(3)
+    URL = f"https://api.github.com/users/{username}"
+    async with aiohttp.ClientSession() as session, session.get(URL) as request:
+        if request.status == 404:
+            return await edit_delete(event, "`" + username + " Not Found`")
+        catevent = await edit_or_reply(event, "`fetching github info ...`")
+        result = await request.json()
+        photo = result["avatar_url"]
+        if result["bio"]:
+            result["bio"] = result["bio"].strip()
+        repos = []
+        sec_res = requests.get(result["repos_url"])
+        if sec_res.status_code == 200:
+            limit = event.pattern_match.group(2)
+            limit = 5 if not limit else int(limit)
+            for repo in sec_res.json():
+                repos.append(f"[{repo['name']}]({repo['html_url']})")
+                limit -= 1
+                if limit == 0:
+                    break
+        REPLY = "**GitHub Info for** `{username}`\
+                \n👤 **Name :** [{name}]({html_url})\
+                \n🔧 **Type :** `{type}`\
+                \n🏢 **Company :** `{company}`\
+                \n🔭 **Blog :** {blog}\
+                \n📍 **Location :** `{location}`\
+                \n📝 **Bio :** __{bio}__\
+                \n❤️ **Followers :** `{followers}`\
+                \n👁 **Following :** `{following}`\
+                \n📊 **Public Repos :** `{public_repos}`\
+                \n📄 **Public Gists :** `{public_gists}`\
+                \n🔗 **Profile Created :** `{created_at}`\
+                \n✏️ **Profile Updated :** `{updated_at}`".format(
+            username=username, **result
+        )
 
-            result = await request.json()
-
-            url = result.get("html_url", None)
-            name = result.get("name", None)
-            company = result.get("company", None)
-            bio = result.get("bio", None)
-            created_at = result.get("created_at", "Not Found")
-
-            REPLY = (
-                f"GitHub Info for `{event.pattern_match.group(1)}`"
-                f"\nUsername: `{name}`\nBio: `{bio}`\nURL: {url}"
-                f"\nCompany: `{company}`\nCreated at: `{created_at}`"
-            )
-
-            if not result.get("repos_url", None):
-                return await event.edit(REPLY)
-            async with session.get(result.get("repos_url", None)) as request:
-                result = request.json
-                if request.status == 404:
-                    return await event.edit(REPLY)
-
-                result = await request.json()
-
-                REPLY += "\nRepos:\n"
-
-                for nr in range(len(result)):
-                    REPLY += f"[{result[nr].get('name', None)}]({result[nr].get('html_url', None)})\n"
-
-                await event.edit(REPLY)
+        if repos:
+            REPLY += "\n🔍 **Some Repos** : " + " | ".join(repos)
+        downloader = SmartDL(photo, ppath, progress_bar=False)
+        downloader.start(blocking=False)
+        await event.client.send_file(
+            event.chat_id,
+            ppath,
+            caption=REPLY,
+            reply_to=reply_to,
+        )
+        os.remove(ppath)
+        await catevent.delete()
 
 
-CMD_HELP.update({
-    "github": f"{cmd}git <nama pengguna>"
-    "\nPenjelasan: Seperti .whois tetapi untuk nama pengguna GitHub."
-})
+CMD_HELP.update(
+    {
+        "github": f"**Plugin : **`github`\
+        \n\n  ⌬  **𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝 :** `{cmd}github` <username>\
+        \n  ⌬  **𝙁𝙪𝙣𝙜𝙨𝙞 : **Menampilkan informasi tentang user di GitHub dari username yang diberikan\
+    "
+    }
+)
