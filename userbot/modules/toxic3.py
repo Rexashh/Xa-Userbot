e09ccd3a99005348bfc4436c24903705eb45b2ef
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-04-08 17:22:20 +0700

    try sambil ditinggal mandi

---
+++ userbot/modules/toxic3.py
@@ -0,0 +1,217 @@
+# © Xa-Userbot
+# Jangan hapus ini ya sayang
+
+from time import sleep
+from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot.utils import Xa_cmd
+
+
+@Xa_cmd(pattern='Y(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Y AJA KONTOL")
+    
+@Xa_cmd(pattern='S(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Sokap lu!!")    
+    
+ @Xa_cmd(pattern='G(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Ga!")  
+    
+    
+@Xa_cmd(Pattern='str(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Stress!!!")
+    
+    
+@Xa_cmd(pattern='nanya(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Nanya mulu lo mmk!!!")
+    
+@Xa_cmd(pattern='usi(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("u siapa?")    
+    
+@Xa_cmd(pattern='oh(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("OH")    
+    
+@Xa_cmd(pattern='terus(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Terus gua harus apa??") 
+
+@Xa_cmd(pattern='??(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("?????")   
+    
+@Xa_cmd(pattern='useh(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("u sehat?")
+    
+    
+@Xa_cmd(pattern='gjls(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Gajelas mmk!") 
+
+
+@Xa_cmd(pattern='ykn(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Y aja ya kntl!")
+
+
+@Xa_cmd(pattern='lunap(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Lu napasi ?")   
+   
+@Xa_cmd(pattern='lontetol(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Lonte Kontol!") 
+    
+    
+@Xa_cmd(pattern='tmo1(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("TMO MULU NGEWE KAGA!") 
+
+@Xa_cmd(pattern='tmo2(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("TMO mulu, sasimo lo y") 
+
+@Xa_cmd(pattern='sasimo1(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Bacot Sasimo")  
+    
+    
+@Xa_cmd(pattern='sasimo2(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Sasimo mmk!")    
+    
+    
+@Xa_cmd(pattern='ngew1(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Dunia isinya Ngewe doang!")
+    
+    
+@Xa_cmd(pattern='alian(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Aliansi Aliansi tai anjing!")    
+    
+    
+@Xa_cmd(pattern='mutual(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("NAK MUTUALAN ABIZZ NIH! SENGGOL DONG🔥")
+    
+@Xa_cmd(pattern='bcmk(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("bct mmk!")
+    
+@Xa_cmd(pattern='alian2(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Anak aliansi y lu?")   
+    sleep(1)
+    await typew.edit("Mau nanya")
+    sleep(1)
+    await typew.edit("RIBUT KOK DISOSMED ?")
+    sleep(1)
+    await typew.edit("AWOAKWOAKWOAK")
+    sleep(1)
+    await typew.edit("Kabur ah...")
+    sleep(1)
+    await typew.edit("Nanti takut Dirosting")
+    sleep(1.)
+    await typew.edit("Terus Nyuruh semua membernya naik ke os")
+    sleep(1.5)
+    await typew.edit("So galak Mic nya ditelen")
+    sleep(1)
+    await typew.edit("Padahal Di Real Life jadi kacung Tongkrongan")
+    sleep(1)
+    await typew.edit("HAHAHA KONTOL!")
+    
+@Xa_cmd(pattern='peting(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("Petinggi Petinggi tai anjing!!") 
+    sleep(1)
+    await typew.edit("Setinggi apasih lu memek?")
+    sleep(1)
+    await typew.edit("Cuma Pengangguran aja belagu lo mmk!!!!")
+    sleep(1)
+    await typew.edit("Kerja bego jangan main tele mulu!")
+    sleep(1)
+    await typew.edit("MEMEK LU!")
+
+
+  
+CMD_HELP.update({
+    "toxic3": f"𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `Y`\
+    \n↳ : coba aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `S{cmd}`\
+    \n↳ : coba aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `G{cmd}`\
+    \n↳ : coba.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `str{cmd}`\
+    \n↳ : coba aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `nanya{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `usi{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `oh{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `terus{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `??{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `useh{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `gjls{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `ykn{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `lunap{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `lontetol{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `tmo1{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `tmo2{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `sasimo1{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `sasimo2{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `ngew1{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `alian{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `mutual{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `bcmk{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `alian2{cmd}`\
+    \n↳ : cobain aja.\
+    \n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `peting{cmd}`\
+    \n↳ : cobain aja."
+})
+    
