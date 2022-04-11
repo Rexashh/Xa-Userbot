ec3ef49cd3a7a9e0e9a6f8aa2bc10c5f744a2296
Rexashh <tirexijo@gmail.com>
2022-04-11 12:10:18 +0000

    pylint: auto fixes
    Signed-off-by: Rexashh <tirexijo@gmail.com>

--- userbot/modules/locks.py
+++ userbot/modules/locks.py
@@ -6,7 +6,7 @@
 
 from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
 from telethon.tl.types import ChatBannedRights
-from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot import CMD_HELP
 from userbot.utils import Xa_cmd
 from userbot import CMD_HANDLER as cmd
 
