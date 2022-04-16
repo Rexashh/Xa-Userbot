121e1f5f331d224ee17db0364a6cb9efe208750d
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-04-16 20:42:35 +0700

    Update meadmin.py

--- userbot/modules/meadmin.py
+++ userbot/modules/meadmin.py
@@ -45,5 +45,5 @@ async def _(event):
         await e1.edit("`Sed, I'm not Admin anywhere 🤧`")
 
 
-CMD_HELP.update({"meadmin": f"\n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `{cmd}meadmin`
+CMD_HELP.update({"meadmin": f"\n\n𝙋𝙚𝙧𝙞𝙣𝙩𝙖𝙝: `{cmd}meadmin`\
                  "\n⌬ Memberikan list group dimana kamu menjadi admin."})
