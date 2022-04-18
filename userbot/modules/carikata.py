a8fe8cea6198b4ab82b8bcb0cd22d2fc1a5027dd
RexAshh <95988728+Rexashh@users.noreply.github.com>
2022-04-18 08:48:02 +0700

    ia

--- userbot/modules/carikata.py
+++ userbot/modules/carikata.py
@@ -13,7 +13,7 @@ from userbot.utils import edit_or_reply, Xa_cmd
 @Xa_cmd(pattern="cari( -r|) ?(.*)")
 async def searcher(e):
     rex = await edit_or_reply(e, "`Mencari Pesan..`")
-    args = edit.pattern_match.group(2)
+    args = e.pattern_match.group(2)
     limit = 5
     if not args or len(args) < 2:
         await edit_or_delete(rex, "Invalid argument!, Coba Lagi")
